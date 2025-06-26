ab0270eab4df95d705d6cef9f6f9517000e3b8a8
HJ <83521479+zodia8393@users.noreply.github.com>
2025-06-27 00:58:51 +0900

    Add files via upload

---
+++ project/food_recommendation_frontend/vue.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        },
+      },
+    },
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Smart Lunch Mate'
+      return args
+    });
+    
+    config.plugin('define').tap(args => {
+      Object.assign(args[0], {
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+      });
+      return args;
+    });
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
+      })
+    ]
+  }
+};
